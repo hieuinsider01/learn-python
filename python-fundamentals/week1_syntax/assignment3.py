739bf704b8110644f1fb7a33520a8d5624fd7dcc
hieuinsider01 <hieuinsider@DESKTOP-FEH88FH.localdomain>
2026-01-25 13:23:40 +0700

    complete assingment 3 and data structure knowledge noted

--- python-fundamentals/week1_syntax/assignment3.py
+++ python-fundamentals/week1_syntax/assignment3.py
@@ -1,8 +1,32 @@
+# Yêu cầu 1:
 authorized_dealers = [
-  dealer_1 = {
-    "name" : "An Phát"
-    "city" : "TPHCM"
-    "rating" : 4.8
-    "product" : ["Anvil 5SC", "Mancozeb", ]
+  {
+    "name" : "An Phát",
+    "city" : "TPHCM",
+    "rating" : "4.8",
+    "product" : ["Anvil 5SC", "Mancozeb", "Amino Humic"]
+  },
+  {
+    "name" : "Nguyễn Công",
+    "city" : "HN",
+    "rating" : "4.5",
+    "product" : ["Ridomil Gold", "Amistar TOP", "Rocsket 888"],
   }
 ]
+
+# Truy cập và in ra tên đại lý thứ nhất:
+dealer_1_name = authorized_dealers[1]["name"]
+print(f"{dealer_1_name} là tên của đại lý thứ hai")
+
+# Add products vào danh sách sản phẩm của đại lý thứ nhất
+add_new_product = authorized_dealers[0]["product"].append("NPK 15-15-15")
+
+#Kiểm tra danh sách mới của product dealer 1
+new_list_product = authorized_dealers[0]["product"]
+print(f'Danh sách các sản phẩm sau khi cập nhật của đại lý 1 là: {new_list_product}')
+
+# Yêu cầu 2:
+
+all_loctations = ["Hà Nội", "TP. HCM", "Đà Nẵng", "Hà Nội", "Cần Thơ", "TP. HCM"]
+
+unique_locations = set(all_loctations)
